bd60925157b8fa9d64ba044f35d5876937741d86
Shobha Yadav <shobhayadav@mac.mynetworksettings.com>
2025-03-07 12:27:18 -0500

    initial setup of the repository and setting launch json for vscode.

---
+++ helloworld/hello.js
@@ -0,0 +1,26 @@
+console.log("hello World")
+
+// const square = function(num){
+//     return num*num;
+// }
+
+// const square = {
+//     area(side){
+//         return side * side;
+//     },
+//     perimeter(side){
+//         return side * 4;
+//     }
+// }
+
+// square.area(4)
+
+
+const hen = {
+    name: 'Helen',
+    eggCount: 0,
+    layAnEgg() {
+        this.eggCount += ;
+        console.log("EGG");
+    }
+}
